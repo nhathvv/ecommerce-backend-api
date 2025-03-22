fae78164f90573e1fd820f22a423b89dc3c13ccd
nhathv <nhathv.21it@gmail.com>
2025-03-22 20:34:00 +0700

    section02

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const morgan = require("morgan");
+const compresion = require("compresion");
+const helmet = require("helmet");
+
+const app = express();
+// init middlwares
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compresion());
+
+// init db
+
+// init router
+app.get("/", (req, res, next) => {
+  const str = "Nhat.dev";
+  return res.status(200).json({
+    message: "Welcome itstahn!!",
+    metadata: str.repeat(100000),
+  });
+});
+// handling error
+
+module.exports = app;
